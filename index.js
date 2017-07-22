4f69c16a9a101de00968e0c315aa0c61eb4ef3c5
sabih <siddiquisabih94@gmail.com>
2017-07-22 15:43:32 +0500

    new server for quiz app

---
+++ index.js
@@ -0,0 +1,103 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+const mongoose = require('mongoose')
+const port = process.env.PORT || '3000';
+const Todo = require('../src/Model/TodoSchema')
+
+mongoose.Promise = global.Promise
+
+mongoose.connect('mongodb://localhost/tododo')
+app.use(bodyParser.json())
+
+
+
+
+app.get('/api', (req, res, next) => {
+
+        Todo.find({})
+            .then(responce => res.send(responce))
+            .catch(next)
+
+        // res.send('saasd')
+    })
+
+
+app.post('/api/todo' , (req, res, next) => {
+
+        const request = req.body;
+        console.log(request)
+        Todo.create(request)
+            .then(responce => res.send(responce))
+            .catch(next)
+    })
+
+
+app.use((err, req, res, next) => {
+    res.send(err.message)
+})
+
+
+
+app.listen(port, function () {
+    console.log("server is listening on port : ", port);
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// app.use((err, req, res, next) => {
+//    console.log(234567876543)
+//     next()
+// })
+
+// Routes(app)
+
+
+// app.listen(1000 , ()=>{
+//     console.log('server is running on port 1000')
+// })
+
+
+// app.get('/api' , (req , res , next)=>{
+//     console.log('chal gya')
+//     res.send('hi there')
+// })
