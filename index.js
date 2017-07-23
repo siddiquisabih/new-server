2237abcfd33adb6e75be7b95a5c4e5b9490ded77
sabih <siddiquisabih94@gmail.com>
2017-07-23 11:31:44 +0500

    debug file added

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser')
 const app = express()
 const mongoose = require('mongoose')
 const port = process.env.PORT || '3000';
-const Todo = require('../src/Model/TodoSchema')
+const Todo = require('./Model/TodoSchema')
 
 mongoose.Promise = global.Promise
 
